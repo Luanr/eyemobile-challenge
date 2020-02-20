8e12ab05be1dbeb358faa97dc5c7cc825d456a6e
Luanr <cbluan@gmail.com>
2020-02-19 22:51:47 -0300

    Added empty routes and server initializing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-import express from 'Express';
+import express from 'express';
+import routes from './routes';
 
-() => {
+const app = express();
+const PORT = 3000;
 
-}
+routes(app);
+
+const initServer = () => {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+};
+
+initServer();
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+const routes = (instance) => {
+};
+
+export default routes;
