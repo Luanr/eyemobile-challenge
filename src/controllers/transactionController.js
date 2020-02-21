1a794f13edc166356b929a5d3c640e3248b8f9c1
Luan <cbluan@gmail.com>
2020-02-20 23:04:51 -0300

    Created transaction controller

---
+++ src/controllers/transactionController.js
@@ -0,0 +1,27 @@
+import Transaction from '../models/transaction';
+
+const getTransactions = (req, res, next) => {
+    try {
+
+    } catch(e) {
+        next(error);
+    }
+};
+
+const getBalance = (req, res, next) => {
+    try {
+
+    } catch(e) {
+        next(error);
+    }
+};
+
+const sendTransaction = (req, res, next) => {
+    try {
+
+    } catch(e) {
+        next(error);
+    }
+}
+
+export default {getTransactions, getBalance, sendTransaction};
