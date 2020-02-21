3d2700e3df688f6fbf3bde62458104e50e19bc6e
Luan <cbluan@gmail.com>
2020-02-20 23:02:15 -0300

    Added initial empty routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
+import controller from './controllers/transactionController';
+
 const routes = (instance) => {
+    instance.get('/transactions', transactionController.getTransactions);
+    instance.get('/balance', transactionController.getBalance);
+    instance.post('/transaction/send', transactionController.sendTransaction);
 };
 
 export default routes;
