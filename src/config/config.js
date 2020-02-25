95f61aec737b1ed161c7554d3874191b712e69ad
Luan <cbluan@gmail.com>
2020-02-25 17:13:36 -0300

    Fixed initial query for database creation on docker

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        host: "db",
-        username: "postgresql",
-        password: "postgresql",
-        database: "eyemobileapi",
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         dialect: 'postgres'
     },
     test: {
--- src/database.js
+++ src/database.js
@@ -3,14 +3,14 @@ import Sequelize from 'sequelize';
 
 Dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize('eyemobileapi', process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres'
 })
 
 const createDb = async () => {
     try {
-        await sequelize.query(`CREATE DATABASE ${process.env.DB_NAME}`);
+        await sequelize.query(`CREATE DATABASE eyemobileapi;`);
     } catch(e) {
     }
 };
